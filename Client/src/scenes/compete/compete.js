12097ab5eb178d5f191e4bfe20a3ee7a1fe59879
LiamMcFadden <liamm18@vt.edu>
2022-04-04 12:25:31 -0400

    top three icons in place, need labels tho

--- Client/src/scenes/compete/compete.js
+++ Client/src/scenes/compete/compete.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import SwitchSelector from 'react-native-switch-selector';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// TODO: replace this with db query of some sort
+import {
+    top10
+} from '../compete/data';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -20,9 +26,32 @@ const styles = StyleSheet.create({
     selector: {
         width: (windowWidth*.75),
         marginLeft: (windowWidth*.25/2)
+    },
+    secondandthird: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    first: {
+        marginTop: 20,
+        alignSelf: 'center'
+    },
+    second: {
+        flex: 1,
+        flexDirection: 'row',
+        textAlign: 'center'
+    },
+    third: {
+        flex: 1,
+        justifyContent: 'space-evenly',
+        textAlign: 'center'
     }
 });
 
+/*
+ * Selector switch for displaying either points or badges
+ * uses 'react-native-switch-selector'
+ */
 const PointsorBadges = () => {
     const options = [
         { label: 'Points', value: 'points' },
@@ -46,9 +75,49 @@ const PointsorBadges = () => {
     );
 }
 
+/*
+ * Displays the pfp/icon for first place user
+ * TODO: add labels to icons
+ */
+const First = () => {
+    return (
+        <View>
+            <Ionicons style={styles.first} name="person-circle-outline" size={50} color={'#0155A4'} />
+        </View>
+    );
+}
+
+/*
+ * Displays the pfp/icon for second and third place user
+ * TODO: add labels to icons
+ */
+const SecondAndThird = () => {
+    return (
+        <View style={styles.secondandthird}>
+            <Ionicons style={styles.second} label="hi" name="person-circle-outline" size={50} color={'#0155A4'}/>
+            <Ionicons style={styles.third} name="person-circle-outline" size={50} color={'#0155A4'} />
+        </View>
+    );
+}
+
+/*
+ * Displays a users name, rank, pfp/icon, and score/badges
+ */
+const PlayerCard = () => {
+    return (
+        <View style={styles.playerCard}>
+        </View>
+    );
+}
+
 const Compete = () => {
     return (
-        <PointsorBadges/>
+        <View>
+            <PointsorBadges/>
+            <First/>
+            <SecondAndThird/>
+            <PlayerCard/>
+        </View>
     );
 }
 
---
+++ Client/src/scenes/compete/data.js
@@ -0,0 +1,16 @@
+module.exports = {
+	top10: [
+		{
+			name: "Jim",
+			rank: 1
+		},
+		{
+			name: "Bob",
+			rank: 2
+		},
+		{
+			name: "Sally",
+			rank: 3
+		}
+	]
+};
