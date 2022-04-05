0f9aafd18cbd85096dbfa8aa1e9c940136b1b361
Jacob <jacobtk@vt.edu>
2022-04-05 08:51:59 -0400

    functions setup

---
+++ Server/functions/src/index.ts
@@ -0,0 +1,15 @@
+import * as functions from "firebase-functions";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+
+export const test = functions.https.onRequest((req: functions.Request, res: functions.Response) => {
+    functions.logger.info("test: ", { hello: "world" })
+    res.send({ hello: "world" })
+})
