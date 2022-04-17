d3cc9397c1282757bcb3282bd42dcb10087864c3
Jacob <jacobtk@vt.edu>
2022-04-17 01:09:50 -0400

    Prepping for scheduled task

--- Server/functions/src/index.ts
+++ Server/functions/src/index.ts
@@ -1,15 +1,46 @@
-import * as functions from "firebase-functions";
+import {
+  EventContext,
+  firestore,
+  pubsub,
+  https,
+  logger,
+} from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const testing = https.onRequest(async (_req, res) => {
+  logger.info("Firestore", {structuredData: true});
+  admin.firestore().collection("test").add({
+    hello: "world",
+  });
+  res.send({hit: "firestore"});
+});
 
 
-export const test = functions.https.onRequest((req: functions.Request, res: functions.Response): void => {
-    functions.logger.info("test: ", { hello: "world" })
-    res.send({ hello: "world" })
-})
+/*
+ * Scheduled task that checks if tournament is over
+ * */
+const createTournamentCountdown =
+  pubsub
+      .schedule("0 0 * * *")
+      .timeZone("America/New_York")
+      .onRun(async (_context: EventContext) => {
+        logger.info({"PUBSUB": "Scheduled Task"});
+
+        // IF TASK DOES NOT EXIST RETURN
+
+        // IF TASK DOES EXIST
+        // CHECK IF END DATE >= TODAYS DATE
+
+        // IF END DATE >= TODAYS DATE
+        //  COMPLETE END TOURNAMENT LOGIC
+        //  DELETE TASK
+      });
+
+export const tournamentCreated = firestore
+    .document("tournaments/{tournamentID}")
+    .onCreate((snap, _context) => {
+      logger.info(snap.data(), {structuredData: true});
+      createTournamentCountdown("Hello", _context);
+    });
