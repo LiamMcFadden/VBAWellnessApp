08f1da139d8064dc4b7f7da91200126b3bd53518
KyleMIrby <kirbyva2@gmail.com>
2022-04-19 23:23:21 -0400

    added the VBA logo to the loading screen

--- Client/src/AuthApp.js
+++ Client/src/AuthApp.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
 import Login from './login/login';
 import BottomTabs from './navigation/app-navigator';
 
@@ -7,10 +7,32 @@ import {UserProvider} from './components/Authentication/user';
 import {subscribeToAuthState, currentUser} from '_api/firebase-auth';
 import {fetch, clear} from '_api/firebase-db';
 
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: 'white',
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 25,
+    paddingTop: 20,
+    //color: '#0155A4',
+    color: 'dimgray',
+    fontWeight: '600',
+    fontStyle: 'italic'
+  }
+});
+
 const LoadingScreen = () => (
-  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text>VBA wellness</Text>
-    <Text>Loading...</Text>
+  // <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+  //   <Text>VBA wellness</Text>
+  //   <Text>Loading...</Text>
+  // </View>
+  <View style={styles.background}>
+    <Image source={require('_assets/images/VBALogo.png')} style={styles.logo}/>
+    <Text style={styles.text}>Wellness Challenge</Text>
   </View>
 );
 export const AppRouter = () => {
